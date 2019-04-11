3082403b66598a786e991c58594ee5d93e513431
jaywood128 <jaywood128@gmail.com>
2019-04-11 14:23:29 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
@@ -12,5 +13,12 @@ class RecipesController < ApplicationController
   end
 
   def create
+   @recipe = Recipe.create(recipe_params)
+   redirect_to recipes_path
   end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
 end
